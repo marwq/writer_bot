7e042d431dd4a1b76b245258e2bead818bd50790
marshq <bauka.alimgazy1@gmail.com>
2024-08-15 06:25:01 +0500

    second commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.filters import CommandStart
+
+load_dotenv()
+
+
+
+client = AsyncOpenAI()
+
+bot = Bot(os.environ["BOT_TOKEN"], default=(DefaultBotProperties(parse_mode='HTML')))
+dp = Dispatcher()
+
+with open('prompt_by_claude.txt', 'r', encoding='utf8') as f:
+    prompt = f.read()
+
+@dp.message(CommandStart())
+async def _(m: types.Message):
+    await m.answer('🇬🇧 Hello! Send me text and i will enhance and format it.\n\n'\
+                    '🇷🇺 Привет! Отправь мне текст и я улучшу и оформлю его.')
+
+@dp.message()
+async def _(m: types.Message):
+    completion = await client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system", 
+                "content": prompt
+            },
+            {
+                "role": "user", 
+                "content": m.text
+            },
+        ]
+    )
+    await m.answer(completion.choices[0].message.content)
+
+asyncio.run(dp.start_polling(bot))
