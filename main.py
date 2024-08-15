485b2e1375c132e51300168a0f59f818403cb8e1
marshq <bauka.alimgazy1@gmail.com>
2024-08-15 06:52:36 +0500

    handle TelegramBadRequest

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, exceptions
 from aiogram.client.default import DefaultBotProperties
 from aiogram.filters import CommandStart
 from httpx import AsyncClient
@@ -11,26 +11,26 @@ from httpx import AsyncClient
 load_dotenv()
 
 
-if os.environ.get('PROXY'):
-    http_client = AsyncClient(proxy=os.environ['PROXY'])
+if os.environ.get("PROXY"):
+    http_client = AsyncClient(proxy=os.environ["PROXY"])
     client = AsyncOpenAI(http_client=http_client)
 else:
     client = AsyncOpenAI()
 
-bot = Bot(os.environ["BOT_TOKEN"], default=(DefaultBotProperties(parse_mode='HTML')))
+bot = Bot(os.environ["BOT_TOKEN"], default=(DefaultBotProperties(parse_mode="HTML")))
 dp = Dispatcher()
 
-with open('prompt_by_claude.txt', 'r', encoding='utf8') as f:
+with open("prompt_by_claude.txt", "r", encoding="utf8") as f:
     prompt = f.read()
 
 @dp.message(CommandStart())
 async def _(m: types.Message):
-    await m.answer('🇬🇧 Hello! Send me text and i will enhance and format it.\n\n'\
-                    '🇷🇺 Привет! Отправь мне текст и я улучшу и оформлю его.')
+    await m.answer("🇬🇧 Hello! Send me text and i will enhance and format it.\n\n"\
+                    "🇷🇺 Привет! Отправь мне текст и я улучшу и оформлю его.")
 
 @dp.message()
 async def _(m: types.Message):
-    msg = await m.answer('One moment...')
+    msg = await m.answer("One moment...")
     completion = await client.chat.completions.create(
         model="gpt-4o",
         messages=[
@@ -44,6 +44,9 @@ async def _(m: types.Message):
             },
         ]
     )
-    await msg.edit_text(completion.choices[0].message.content)
+    try:
+        await msg.edit_text(completion.choices[0].message.content)
+    except exceptions.TelegramBadRequest:
+        await msg.edit_text("❌ LLM generated invalid format. Try again...")
 
 asyncio.run(dp.start_polling(bot))
